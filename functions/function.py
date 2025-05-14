5491cbdd863ae53b1022575564401fc2981691c2
Pranshu Gajera <pranshugajra77@gmail.com>
2025-05-14 20:24:08 +0530

    fun

---
+++ functions/function.py
@@ -0,0 +1,13 @@
+# +++++++++++++++++++++++ function definition ++++++++++++++++++++++++
+def avg():
+    a = int(input("Enter the number: "))
+    b = int(input("Enter the number: "))
+    c = int(input("Enter the number: "))
+    print("the average of this number is: ", (a + b + c)/3)
+    
+# +++++++++++++++++++++++++ function call +++++++++++++++++++++++++++++
+avg()
+avg()
+avg()
+avg()
+avg()
